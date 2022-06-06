78b1539bcc4079a5e6cd3fde9adc79151de37c30
Antonio Emerson <emerson.nessa@gmail.com>
2022-06-06 19:12:54 -0300

    atualizado alert

--- script.js
+++ script.js
@@ -18,27 +18,4 @@ if (alertTrigger) {
   })
 }
 
-//
 
-
-const alertPlaceholder2 = document.getElementById('liveAlertPlaceholder2','liveAlertPlaceholder3', 'liveAlertPlaceholder4')
-
-const alert2 = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-
-}
-
-const alertTrigger2 = document.getElementById('liveAlertBtn2', 'liveAlertBtn3', 'liveAlertBtn4')
-if (alertTrigger2) {
-  alertTrigger2.addEventListener('click', () => {
-    alert2('Excluido com sucesso!', 'danger')
-  })
-}
