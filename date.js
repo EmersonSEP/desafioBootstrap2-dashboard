dfb2e7c5e5601ea8f10330401b2f5d98ecdbe5c2
Antonio Emerson <emerson.nessa@gmail.com>
2022-06-09 18:09:43 -0300

    adicionado toast e estilizacao em css

---
+++ date.js
@@ -0,0 +1,3 @@
+const hoje = new Date();
+
+console.log(dateFns.format(hoje, 'DD/MM/YYYY')); 
--- script.js
+++ script.js
@@ -1,21 +1,68 @@
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const btnToast = document.getElementById('ad-toast');
 
-const alert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+btnToast.addEventListener("click", () => (
+  Toastify({
+    text: "Cadastrado com sucesso!",
+    gravity: "top",
+    position: "center",
+    className: "info",
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    }
+  }).showToast()
+)
+);
 
-  alertPlaceholder.append(wrapper)
-}
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    alert('Enviado com sucesso!', 'success')
-  })
-}
+const btnToast2 = document.getElementById('ad-toast2');
+btnToast2.addEventListener("click", () => (
+  Toastify({
+    text: "Excluido com sucesso!",
+    gravity: "top",
+    position: "center",
+    className: "info",
+    style: {
+      background: "linear-gradient(to right, rgb(205, 8, 8), rgb(34, 2, 2))",
+    }
+  }).showToast()
+)
+);
+const btnToast3 = document.getElementById('ad-toast3');
+btnToast3.addEventListener("click", () => (
+  Toastify({
+    text: "Excluido com sucesso!",
+    gravity: "top",
+    position: "center",
+    className: "info",
+    style: {
+      background: "linear-gradient(to right, rgb(205, 8, 8), rgb(34, 2, 2))",
+    }
+  }).showToast()
+)
+);
+const btnToast4 = document.getElementById('ad-toast4');
+btnToast4.addEventListener("click", () => (
+  Toastify({
+    text: "Excluido com sucesso!",
+    gravity: "top",
+    position: "center",
+    className: "info",
+    style: {
+      background: "linear-gradient(to right, rgb(205, 8, 8), rgb(34, 2, 2))",
+    }
+  }).showToast()
+)
+);
+
+
+$('.use-slick').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+
+
 
 
